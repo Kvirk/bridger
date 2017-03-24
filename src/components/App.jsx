4d4a3026e6b708bbed0f168d64dad9643fff2854
Kirat Virk <kirat_virk3@yahoo.ca>
2017-03-24 22:16:57 +0000

    logic for profile page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import '../assets/stylesheets/base.scss';
 import React, { Component } from 'react';
 import LinkedinLogin from './LinkedInLogin.jsx';
+import Event from './Event.jsx';
 import cookie from 'react-cookie';
 
 let socket = io.connect();
@@ -8,11 +9,22 @@ let socket = io.connect();
 class App extends Component {
   constructor(props) {
     super(props);
-    this.submit = this.submit.bind(this);
+
+    let type = 'login';
+    let data = {};
+    if (cookie.load('userId')){
+      type = 'events';
+      data = {
+          myEvent:['a','b', 'c'],
+          allEvent: ['d', 'e' ]
+        };
+    }
+    this.addEvent = this.addEvent.bind(this);
+    this.eventPage = this.eventPage.bind(this);
     this.onLogout = this.onLogout.bind(this);
     this.callbackFunction = this.callbackFunction.bind(this);
-    this.state = {type: "login",
-          data: {},
+    this.state = {type: type,
+        data: data,
         userId: cookie.load('userId'),
         name: cookie.load('name')}
   }
@@ -28,6 +40,11 @@ class App extends Component {
       cookie.save('userId', data.id, { path: '/' });
       cookie.save('name', data.firstName, { path: '/' });
       app.setState({
+        type: 'events',
+        data: {
+          myEvent:['a','b', 'c'],
+          allEvent: ['d', 'e' ]
+        },
         userId: data.id,
         name: data.firstName});
     }
@@ -38,34 +55,46 @@ class App extends Component {
     IN.API.Raw("/people/~:(id,first-name,last-name,location,positions,industry,specialties,summary)?format=json").result(onSuccess).error(onError);
   }
 
+  addEvent(event){
+    let myEvent = this.state.data.myEvent;
+    let allEvent = this.state.data.allEvent;
+    myEvent.push(event);
+
+    let index = allEvent.indexOf(event);
+
+    if (index > -1) {
+      allEvent.splice(index, 1);
+    }
+
+    this.setState({data: {
+          myEvent,
+          allEvent
+        }});
+  }
+
+  eventPage(event){
+    console.log(event)
+  }
+
   onLogout() {
     cookie.remove('userId', { path: '/' });
     cookie.remove('name', { path: '/' });
     this.setState({
+        type: 'login',
+        data: {},
         userId: null,
         name: null});
-  }
-
-  submit(key) {
-    socket.emit('type', 'thiu');
-    if (key.charCode === 13) {
-      const newMessage = key.target.value;
-      key.target.value = '';
-      console.log(this.state.test)
-      this.setState({test: "newMessage" })
-      console.log(this.state.test)
-    }
   }
 
   render() {
     if (!this.state.userId) {
       return <LinkedinLogin callbackFunction={this.callbackFunction}/>;
     }
+    if (this.state.type === "events"){
+       return <Event name={this.state.name} eventPage={this.eventPage} addEvent={this.addEvent} data={this.state.data} onLogout={this.onLogout}/>
+    }
     return (
-      <div>
-        <h1>Hello, {this.state.name}!</h1>
-        <button onClick={this.onLogout}> Log Out</button>
-      </div>
+      <h1>ERROR</h1>
     )
   }
 };
