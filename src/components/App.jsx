dddbd75916feede2cf2283d51e95b382ae8aa603
richard135 <richardhsieh1@hotmail.com>
2017-03-26 21:31:08 +0000

    Switching branches to fix login

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,12 @@
 import React, { Component } from 'react';
 import NavBar from './NavBar.jsx';
+import MainSection from './MainSection.jsx';
+import UserProfile from './UserProfile.jsx';
 import LinkedinLogin from './LinkedInLogin.jsx';
+import EventProfile from './EventProfile.jsx';
+import Event from './Event.jsx';
 import cookie from 'react-cookie';
 import EventsCreation from './EventsCreation.jsx';
-
-import Welcome from './Welcome.jsx';
-import AllEvents from './AllEvents.jsx';
-import MyEvents from './MyEvents.jsx';
-import EventProfile from './EventProfile.jsx';
-import AllPeople from './AllPeople.jsx';
-import PersonProfile from './PersonProfile.jsx';
-import Schedule from './Schedule.jsx';
-
-// import UserProfile from './UserProfile.jsx';
-// import EventProfile from './EventProfile.jsx';
-// import Event from './Event.jsx';
 
 
 let socket = io.connect();
@@ -38,20 +30,18 @@ class App extends Component {
     this.eventPage = this.eventPage.bind(this);
     this.onLogout = this.onLogout.bind(this);
     this.callbackFunction = this.callbackFunction.bind(this);
-    this.goToEventProfile = this.goToEventProfile.bind(this);
-    this.goHome= this.goHome.bind(this);
-    this.onLogin = this.onLogin.bind(this);
     this.eventsCreation = this.eventsCreation.bind(this);
     this.handleForm = this.handleForm.bind(this);
     this.state = {type: type,
         userId: cookie.load('userId'),
-        name: cookie.load('name')
-      }
+        name: cookie.load('name')}
   }
 
   componentDidMount() {
     const app = this;
-    socket.on('connect', function(data) {});
+    socket.on('connect', function(data) {
+
+    });
     socket.on('responseUserLogin', function(data) {
       app.setState({
         type: 'events',
@@ -66,25 +56,6 @@ class App extends Component {
       }
       socket.emit('userLogin', data2)
     });
-    // LinkedIn Login
-    let liRoot = document.createElement('div');
-      liRoot.id = 'linkedin-root';
-
-      document.body.appendChild(liRoot);
-
-      (function(d, s, id) {
-        const element = d.getElementsByTagName(s)[0];
-        const ljs = element;
-        var js = element;
-        if (d.getElementById(id)) {
-            return; }
-        js = d.createElement(s);
-        js.id = id;
-        js.src = '//platform.linkedin.com/in.js';
-        js.text = 'api_key: 86ihm2bra9vjg3';
-        ljs.parentNode.insertBefore(js, ljs);
-      }(document, 'script', 'linkedin-sdk'));
-    //End of LinkedIn Login
   }
 
   callbackFunction() {
@@ -160,6 +131,7 @@ class App extends Component {
     });
   }
 
+
   backToEP(){
     this.setState({
       type: 'event',
@@ -171,109 +143,40 @@ class App extends Component {
         }
     });
   }
-  // TODO still cant login using LinkedIn
-  onLogin() {
-    console.log("State is about to change to logged in");
-    this.setState({
-      type: 'loggedin'
-    })
-    // function onSuccess(data) {
-    //   socket.emit('user', data)
-    //   cookie.save('userId', data.id, { path: '/' });
-    //   cookie.save('name', data.firstName, { path: '/' });
-    //   this.setState({
-    //     type: 'loggedin',
-    //     data: {
-    //       myEvent:['a','b', 'c'],
-    //       allEvent: ['d', 'e' ]
-    //     },
-    //     userId: data.id,
-    //     name: data.firstName});
-    // }
-
-    // function onError(error) {
-    // }
-
-    // IN.API.Raw("/people/~:(id,first-name,last-name,headline,location,industry,current-share,num-connections,summary,positions,picture-urls::(original),public-profile-url)?format=json").result(onSuccess).error(onError);
-  }
 
   onLogout() {
     cookie.remove('userId', { path: '/' });
     cookie.remove('name', { path: '/' });
     this.setState({
-        type: 'home',
+        type: 'login',
         data: {},
         userId: null,
         name: null});
   }
 
-
-  goToEventProfile() {
-    console.log("State is about to change to testEvent");
-    this.setState({
-      type: 'event'
-    })
-  }
-
-  goHome() {
-    console.log("State is about to change to home");
-    this.setState({
-      type: 'home'
-    })
-  }
-
   render() {
 
-    let topSectionPartial;
-    let bottomSectionPartial;
-    switch (this.state.type) {
-      // When logging in
-      case 'loggedin':
-        console.log("Section, logged in state");
-        topSectionPartial = <MyEvents goToEventProfileHandler={this.goToEventProfile} />;
-        bottomSectionPartial = <AllEvents />;
-        break;
-      // When clicking a specific event in MyEvent section
-      case 'event':
-        console.log("Section, event state");
-        topSectionPartial = <AllPeople goToPersonProfileHandler={this.seeProfile} />;
-        bottomSectionPartial = <Schedule />;
-        break;
-      case 'userProfile':
-        console.log("Section, userProfile state");
-
-      // Home page partials
-      default:
-        console.log("Section, home state");
-        topSectionPartial = <Welcome callbackFunction={this.callbackFunction} />;
-        bottomSectionPartial = <AllEvents />;
-    }
     if (this.state.type === "creation") {
        return <EventsCreation  handleForm={this.handleForm}/>
-     }
+    }
 
     if (!this.state.userId) {
       return (
-      // TODO refactor props (i.e. store handler functions into one 'handlers' object)
-      <div className="container">
-        <NavBar urlPath={this.state.type} goHomeHandler={this.goHome} loginHandler={this.onLogin} logoutHandler={this.onLogout} eventsCreationFunction={this.eventsCreation} />
-        <section className="top-section row">
-          {topSectionPartial}
-        </section>
-        <section className="bottom-section row">
-          {bottomSectionPartial}
-        </section>
+      <div>
+        <NavBar eventsCreationFunction={this.eventsCreation}/>
+        <MainSection callbackFunction={this.callbackFunction}/>
       </div>
     )}
-    // if (this.state.type === "events"){
-    //    return <Event name={this.state.name} eventPage={this.eventPage} addEvent={this.addEvent} data={this.state.data} onLogout={this.onLogout}/>
-    // }
-    // if (this.state.type === "event"){
-    //    return <EventProfile name={this.state.name} seeProfile={this.seeProfile} backToMain={this.backToMain} data={this.state.data} onLogout={this.onLogout}/>
-    // }
-    // if (this.state.type === "userProfile"){
-    //    return <UserProfile name={this.state.name} backToEP={this.backToEP} data={this.state.data} onLogout={this.onLogout}/>
-    // }
+
+    if (this.state.type === "events"){
+       return <Event name={this.state.name} eventPage={this.eventPage} addEvent={this.addEvent} data={this.state.data} onLogout={this.onLogout}/>
+    }
+    if (this.state.type === "event"){
+       return <EventProfile name={this.state.name} seeProfile={this.seeProfile} backToMain={this.backToMain} data={this.state.data} onLogout={this.onLogout}/>
+    }
+    if (this.state.type === "userProfile"){
+       return <UserProfile name={this.state.name} backToEP={this.backToEP} data={this.state.data} onLogout={this.onLogout}/>
+    }
     return (
       <h1>ERROR</h1>
     )
--- src/components/EventProfile.jsx
+++ src/components/EventProfile.jsx
@@ -13,13 +13,13 @@ class EventProfile extends Component {
     return (
       <div>
         <h1>This is from EventProfile</h1>
-        {/*<h1>Hello, {this.props.name}!</h1>
+        <h1>Hello, {this.props.name}!</h1>
         <button onClick={this.props.onLogout}> Log Out</button>
         <h3>{this.props.data.name}</h3>
         <h3>{this.props.data.timeStart}</h3>
         <h3>{this.props.data.timeEnd}</h3>
         <h3 onClick={this.props.seeProfile}>{this.props.data.people[1]}</h3>
-        <button onClick={this.props.backToMain}> Back</button>*/}
+        <button onClick={this.props.backToMain}> Back</button>*
       </div>
     )
   }
