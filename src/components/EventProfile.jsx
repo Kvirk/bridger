7788e5df4429ec552d3e63957b319e3e747f2823
richard135 <richardhsieh1@hotmail.com>
2017-03-29 01:15:09 +0000

    Styled top 5 people

--- src/components/EventProfile.jsx
+++ src/components/EventProfile.jsx
@@ -16,12 +16,39 @@ class EventProfile extends Component {
     let dateEnd = new Date(this.props.data.event.end_time)
     let length = this.props.data.users.length
     return (
-      <div>
+      <div className="event-profile-container">
         <header className="header-image">
           <div className="container">
             <div className="featurette" classID="about">
               <h2 className="featurette-heading">{this.props.data.event.name},
                 <div className="text-muted">Hello, {this.props.name}!</div>
+                  {this.props.data.users.map((dat, i) => {
+                    return (
+                      <div className="col-lg-15 .col-xs-15 .col-sm-15 .col-md-15">
+                        <div className="center-block">
+                          <Card style={{width: '225px'}}>
+                            <CardTitle/>
+                            <CardMedia
+                              aspectRatio="square"
+                              image="https://placeimg.com/800/450/nature"
+                            />
+                            <CardTitle
+                              key={i}
+                              onClick={this.props.seeProfile.bind(null, dat)}
+                              title={dat.first_name}
+                              subtitle="Subtitle here"
+                            />
+                            <CardText></CardText>
+                            <CardActions>
+                              <Button label="Action 1" />
+                              <Button label="Action 2" />
+                            </CardActions>
+                          </Card>
+                        </div>
+                      </div>
+                    )
+                  })}
+              <hr className="featurette-divider"/>
               </h2>
               <p className="lead text-muted">
                 Bridged helps you reach your full potential by connecting you with the right people.
@@ -37,41 +64,10 @@ class EventProfile extends Component {
               </p>
             </div>
           </div>
+          <Button  icon='back' onClick={this.props.backToMain} raised />
+          <Button  icon='logout'onClick={this.props.onLogout} raised/>
           <hr className="featurette-divider"/>
         </header>
-          {this.props.data.users.map((dat, i) => {
-            return (
-              <div className={"col-md-" + (12/length).toString()}>
-                <div className="center-block">
-                  <Card style={{width: '350px'}}>
-                    <CardTitle
-                      avatar="https://placeimg.com/80/80/animals"
-                      title="Avatar style title"
-                      subtitle="Click Above to enter profile"
-                    />
-                    <CardMedia
-                      aspectRatio="wide"
-                      image="https://placeimg.com/800/450/nature"
-                    />
-                    <CardTitle
-                      key={i}
-                      onClick={this.props.seeProfile.bind(null, dat)}
-                      title={dat.first_name}
-                      subtitle="Subtitle here"
-                    />
-                    <CardText></CardText>
-                    <CardActions>
-                      <Button label="Action 1" />
-                      <Button label="Action 2" />
-                    </CardActions>
-                  </Card>
-                </div>
-              </div>
-            )
-          })}
-        <hr className="featurette-divider"/>
-        <Button  icon='back' onClick={this.props.backToMain} raised />
-        <Button  icon='logout'onClick={this.props.onLogout} raised/>
       </div>
     )
   }
