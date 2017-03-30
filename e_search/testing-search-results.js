45ec5d4e5db80ffccd15e47c168f4732693ba58b
Hans Kurniadi <hans.d.k@live.com>
2017-03-30 01:05:11 +0000

    update and insert search result and points into database

--- e_search/testing-search-results.js
+++ e_search/testing-search-results.js
@@ -13,37 +13,56 @@ const knex = require('knex')({
   }
 });
 
-let aggregateResults = [];
 
-// function collectResults(queryResults) {
-//   console.log('------->', queryResults);
-//   aggregateResults.push(queryResults);
-//   // console.log('Aggregated Results', aggregateResults);
-// }
+let updateUserPoints = (matchResult) => {
+  console.log("Incoming results", matchResult);
+  console.log("Updating points...");
+  knex('points').where({
+    user_id1: 3,
+    user_id2: matchResult.userId
+  }).increment('points', matchResult.matchingScore)
+  .then((result) => {
+    console.log('Update result', result);
+    if (result === 0) {
+      knex('points').returning('user_id1', 'user_id2', 'points').insert({
+        user_id1: 3,
+        user_id2: matchResult.userId,
+        points: matchResult.matchingScore
+      }).then((output) => {
+        console.log("Output: ", output);
+        knex.destroy();
+      }, (err) => {
+        console.log(err);
+        knex.destroy();
+      });
+    }
+  }, (err) => {
+    console.log(err);
+  });
+};
 
 let consumeResult = (results) => {
-  console.log('---->', results.hits);
-  // let searchResults = [];
   console.log(`found ${results.hits.total} items in ${results.took}ms`);
   if (results.hits.total > 0) {
     results.hits.hits.forEach((hit, index) => {
+      console.log("User id", hit._source.id);
       console.log(`\t ${++index} - ${hit._source.first_name} - ${hit._source.linkedin_id} \n Summary: ${hit._source.summary} \n Industry: ${hit._source.industry} \n Score: ${hit._score} \n`);
-      // searchResults.push({
-      //   linkedin_id: hit._source.linkedin_id,
-      //   matching_score: hit._score
-      // });
-      // let searchResults = {
-      //   linkedin_id: hit._source.linkedin_id,
-      //   matching_score: hit._score
-      // }
-      // collectResults(searchResults);
+      let searchResults = {
+        userId: hit._source.id,
+        matchingScore: hit._score
+      }
+      updateUserPoints(searchResults);
     });
   }
 };
 
+let handleError = (err) => {
+  console.log(err);
+}
 
-// elasticSearch.invokeSearch('Senior software engineer seeking new position, Chief Technology Officer at Microsoft previously.', ['summary', 'industry'])
-// .then(consumeResult, (err) => {
-//   console.log(err);
-// });
-
+knex.select().from('users').where('id', 2)
+  .then((user) => {
+    let queryValue = user[0].industry;
+    elasticSearch.invokeSearch(queryValue, ['summary', 'industry'])
+      .then(consumeResult, handleError);
+  });
