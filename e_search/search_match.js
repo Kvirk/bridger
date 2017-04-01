6c1b2e69b6f80a3110e8682ffc374820167ed641
richard135 <richardhsieh1@hotmail.com>
2017-04-01 21:57:37 +0000

    Adding should & Must to the elastic search

--- e_search/search_match.js
+++ e_search/search_match.js
@@ -16,23 +16,33 @@
   // all calls should be initiated through the module
   const test = function test() {
     let body = {
-      size: 20,
-      from: 0,
+      // size: 20,
+      // from: 0,
       query: {
-        multi_match: {
-          query: 'software',
-          fields: ['summary', 'industry'],
-          fuzziness: 'auto'
-        }
+        // bool: {
+          // must: {
+            match: {
+              "_all": "software engineer"
+              // query: 'software',
+              // fields: ['summary', 'industry', 'headline', 'location', 'current_share'],
+              // fuzziness: 'auto'
+            }
+          // }
+          // should: {
+          //   match_phrase:{
+          //     "_all":"software engineer"
+          //   }
+          // }
+        // }
       }
     };
 
-    console.log(`retrieving documents whose summary and headline matches phrase '${body.query.multi_match.query}' (displaying ${body.size} items at a time)...`);
+    console.log(`retrieving documents whose summary and headline matches phrase '${body.query}' (displaying ${body.size} items at a time)...`);
     search('users', body)
     .then(results => {
       console.log(`found ${results.hits.total} items in ${results.took}ms`);
       if (results.hits.total > 0) console.log(`returned article summary:`);
-      results.hits.hits.forEach((hit, index) => console.log(`\t ${hit._source.first_name} - ${body.from + ++index} - ${hit._source.headline} - ${hit._source.summary}  (score: ${hit._score})`));
+      results.hits.hits.forEach((hit, index) => console.log(`\t ${hit._source.first_name} - ${hit._source.headline} - ${hit._source.summary}  (score: ${hit._score})`));
     })
     .catch(console.error);
   };
