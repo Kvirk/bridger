add4d9a2a41c4aadd01dcf9f7193ff5c69087852
richard135 <richardhsieh1@hotmail.com>
2017-04-05 06:06:36 +0000

    Added up to 20 seeds, deleted david & hans profile

--- seeds/03_event_users.js.js
+++ seeds/03_event_users.js.js
@@ -30,6 +30,66 @@ exports.seed = function(knex, Promise) {
           user_id: 6
         }),
         knex('event_users').insert({
+          event_id: 1,
+          user_id: 7
+        }),
+        knex('event_users').insert({
+          event_id: 1,
+          user_id: 8
+        }),
+        knex('event_users').insert({
+          event_id: 1,
+          user_id: 9
+        }),
+        knex('event_users').insert({
+          event_id: 1,
+          user_id: 10
+        }),
+        knex('event_users').insert({
+          event_id: 1,
+          user_id: 11
+        }),
+        knex('event_users').insert({
+          event_id: 1,
+          user_id: 12
+        }),
+        knex('event_users').insert({
+          event_id: 1,
+          user_id: 13
+        }),
+        knex('event_users').insert({
+          event_id: 1,
+          user_id: 14
+        }),
+        knex('event_users').insert({
+          event_id: 1,
+          user_id: 15
+        }),
+        knex('event_users').insert({
+          event_id: 1,
+          user_id: 16
+        }),
+        knex('event_users').insert({
+          event_id: 1,
+          user_id: 17
+        }),
+        knex('event_users').insert({
+          event_id: 1,
+          user_id: 18
+        }),
+        knex('event_users').insert({
+          event_id: 1,
+          user_id: 19
+        }),
+        knex('event_users').insert({
+          event_id: 1,
+          user_id: 20
+        }),
+        knex('event_users').insert({
+          event_id: 1,
+          user_id: 21
+        }),
+        knex('event_users').insert({
           event_id: 2,
           user_id: 1
         }),
